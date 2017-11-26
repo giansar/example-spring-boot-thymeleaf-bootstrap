9cfe0f6a1804c71d84e01d8aa8e653582fc024cd
Giansar Pratiknya <gpratiknya@gmail.com>
2017-11-26 10:34:24 +0700

    Initial Commit

---
+++ src/main/java/id/giansar/example/ExampleSpringBootThymeleafBootstrapApplication.java
@@ -0,0 +1,12 @@
+package id.giansar.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootThymeleafBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootThymeleafBootstrapApplication.class, args);
+	}
+}
